6a4f816c34d99024c05cb95e954211315c881766
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-01-04 19:31:55 -0300

    Recebendo cpf via headers

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ app.post("/account", (req, res) => {
     return res.status(201).send()
 })
 
-app.get("/statement/:cpf", (req, res) => {
-    const { cpf } = req.params
+app.get("/statement/", (req, res) => {
+    const { cpf } = req.headers
     const customer = customers.find(customer => customer.cpf === cpf)
     if(!customer) {
         return res.status(400).json({ error: 'Customer not found!' })
