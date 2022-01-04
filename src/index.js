1486ac91f9b5e8300bf92123d971698251d51b28
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-01-04 18:39:10 -0300

    Criação de conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const { v4: uuidV4 } = require('uuid')
 
 const app = express()
+app.use(express.json())
+
+const customers = []
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body
+    const id = uuidV4()
+    customers.push({ id, name, cpf, statement: [] })
+    return res.status(201).send()
+})
 
 app.listen(3333)
